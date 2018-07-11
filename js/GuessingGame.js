e486b26d4c2997d20f6def322b084e9ec6f3fd0e
Andy Poon <anpoon430@gmail.com>
2018-07-12 01:50:02 +0800

    finished basic functionality

--- js/GuessingGame.js
+++ js/GuessingGame.js
@@ -1,10 +1,38 @@
+/*eslint-disable*/
 $(document).ready(function(){
-  var newGame = new Game();
-  $("#submit").click(function(){;
-    var outcome = newGame.playersGuessSubmission(+$('#player-input').val());
-    console.log(outcome);
+  var game = new Game();
+  var originalTitle = $('#title').text();
+  var originalSubTitle = $('#subtitle').text();
+  var getOutcome = function(){
+    var guess = +$('#player-input').val();
+    var outcome = game.playersGuessSubmission(guess);
+    $('#player-input').val('');
+    $('#title').text(outcome);
+  }
+  var reset = function(){
+    $('#submit, #hint').prop('disabled', false);
+    game = new Game();
+    $('#player-input').prop('autofocus', true);
+    $('#guesses ul li').text('-');
+    $('#title').text(originalTitle);
+    $('#subtitle').text(originalSubTitle);
+  }
+  $("#submit").click(getOutcome);
+  $(window).keypress(function(){
+    if (event.which === 13 ) getOutcome();
   });
-})
+
+  $("#reset").click(reset);
+
+  $('#hint').click(function(){
+    var hint = game.provideHint();
+    $('#rightSide').slideDown();
+    $('#rightSide').text('Winning numbers are : ' + hint.join(', '));
+    setTimeout(function(){
+      $('#rightSide').fadeOut();
+    }, 5000);
+  });
+});
 
 function generateWinningNumber(){
   var max = 101; //exclusive upper range
@@ -49,16 +77,25 @@ Game.prototype.playersGuessSubmission = function(num){
 
 Game.prototype.checkGuess = function(){
   if (this.playersGuess === this.winningNumber){
+    $('#submit, #hint').prop('disabled', true);
+    $('#subtitle').text('Press reset to play again');
     return 'You Win!';
   } if (this.pastGuesses.indexOf(this.playersGuess) !== -1){
-    return 'You have already guessed that number.';
+      $('#subtitle').text('Please try again');
+      return 'You have already guessed that number.';
   }
   this.pastGuesses.push(this.playersGuess);
+  $('#guesses ul li').eq(this.pastGuesses.length - 1).text(this.playersGuess);
   if (this.pastGuesses.length === 5){
-    return 'You Lose.';
+    $('#subtitle').text('Press reset to play again.');
+    $('#submit, #hint').prop('disabled', true);
+    return 'You lose! The Winning Number was ' + this.winningNumber;
   }
   var diff = this.difference();
-  if (diff < 10) return "You're burning up!";
+  if (this.isLower()) $('#subtitle').text('Guess higher!');
+  else $('#subtitle').text('Guess lower!');
+
+  if (diff < 10)  return "You're burning up!";
   if (diff < 25) return "You're lukewarm.";
   if (diff < 50) return "You're a bit chilly.";
   if (diff < 100) return "You're ice cold!";
